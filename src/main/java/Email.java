18fc7e2e29fd58a46d31c3381a93d8cb655f2830
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-02-19 16:48:46 -0500

    Department information request added

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -7,8 +9,33 @@ public class Email {
     private String alternateEmail;
 
     //Need constructor to receive the first name and last name of the user
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email created for: " + this.firstName + " " + this.lastName);
+
+        //Now call method asking for dept and returning department
+        this.department = setDepartment();
+        System.out.println("Department chosen is: " + this.department);
+    }
 
     //Need to ask what department they are in
+    private String setDepartment(){
+        System.out.println("""
+                Enter the department code for employee
+                1 for Sales\s
+                2 for Development
+                3 for Accounting
+                0 for none\s""");
+        Scanner input = new Scanner(System.in);
+        int deptChoice = input.nextInt();
+        return switch (deptChoice) {
+            case 1 -> "Sales";
+            case 2 -> "Development";
+            case 3 -> "Accounting";
+            default -> "";
+        };
+    }
 
     //Generate a random password
 
--- src/main/java/EmailApp.java
+++ src/main/java/EmailApp.java
@@ -2,5 +2,6 @@ public class EmailApp {
 
     public static void main(String[] args) {
         // main application class
+        Email email1 = new Email("Ryan", "Smith");
     }
 }
